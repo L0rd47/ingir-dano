6b9afd62702b1285f7913d5b7a33a8541d0d237c
Александр Колесников <76116048+L0rd47@users.noreply.github.com>
2023-11-15 01:43:06 +0500

    Add files via upload

---
+++ ingir dano/Practice2/2/task_2.py
@@ -0,0 +1,12 @@
+import numpy as np
+import os
+
+VAR = 56
+matrix = np.load('matrix_56_2.npy')
+x, y = np.where(matrix > 500 + VAR)
+z = matrix[matrix > 500 + VAR]
+np.savez('t2_result.npz', x=x, y=y, z=z)
+np.savez_compressed('t2_result_compressed.npz', x=x, y=y, z=z)
+
+print("Размер оригинального файла: " + str(os.path.getsize('t2_result.npz')))
+print("Размер сжатого файла: " + str(os.path.getsize('t2_result_compressed.npz')))
---
+++ ingir dano/Practice2/3/task_3.py
@@ -0,0 +1,35 @@
+import json
+import msgpack
+import os
+
+file = open('products_56.json')
+json_data = json.load(file)
+file.close()
+
+result = {}
+for item in json_data:
+    name = item["name"]
+    price = item["price"]
+    if name in result:
+        data = result[name]
+        data["count"]+=1
+        data["summ"] += price
+        data["max_price"] = max(data["max_price"], price)
+        data["min_price"] = min(data["min_price"], price)
+    else:
+        result[name] = {"average_price": price, "max_price": price, "min_price": price, "count": 1, "summ": int(price)}
+
+result1 ={}
+for item in result:
+    result[item]["average_price"] = result[item]["summ"] / result[item]["count"]
+    result1[item] = {"average_price": result[item]["average_price"], "max_price": result[item]["max_price"], "min_price": result[item]["min_price"]}
+file = open("t3_result.json", "w")
+json.dump([{"name": name, **data} for name, data in result1.items()], file, indent=2)
+file.close()
+
+file = open("t3_result.msgpack", "wb")
+file.write(msgpack.packb(result1))
+file.close()
+
+print("Размер оригинального файла: " + str(os.path.getsize('t3_result.json')))
+print("Размер сжатого файла: " + str(os.path.getsize('t3_result.msgpack')))
